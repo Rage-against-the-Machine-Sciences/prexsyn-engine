ad3adc6e8c3e2dc2457dd666a10b66e0352a6e1c
Shitong Luo <luost26@gmail.com>
2026-03-19 11:07:20 -0400

    minor config updates

--- third_party/boost.sh
+++ third_party/boost.sh
@@ -9,7 +9,7 @@ fi
 
 mkdir -p "$PREFIX"
 
-LIBRARIES=(serialization iostreams program_options)
+LIBRARIES=(serialization iostreams program_options json)
 
 if [[ -d "boost" ]]; then
     echo "Boost directory already exists. Skipping clone."
