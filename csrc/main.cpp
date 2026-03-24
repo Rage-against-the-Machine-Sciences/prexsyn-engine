89cdcb4bded6dbbfa2e2ebf17a02d82eb0bb0781
Shitong Luo <luost26@gmail.com>
2026-03-24 11:33:24 -0400

    update example

--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,11 +1,46 @@
+#include <chrono>
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <string>
 
-#include <GraphMol/GraphMol.h>
-#include <GraphMol/SmilesParse/SmilesParse.h>
+#include "chemspace/chemspace.hpp"
+#include "datapipe/datapipe.hpp"
+#include "descriptor/descriptor.hpp"
 
-int main() {
-    std::unique_ptr<RDKit::ROMol> mol(RDKit::SmilesToMol("CCO"));
-    std::cout << "Molecule: " << mol->getNumAtoms() << " atoms\n";
+using namespace prexsyn;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <chemical_space_file>\n";
+        return 1;
+    }
+    std::string cs_path = argv[1];
+    std::ifstream ifs(cs_path, std::ios::binary);
+    if (!ifs) {
+        std::cerr << "Failed to open chemical space file: " << cs_path << "\n";
+        return 1;
+    }
+    std::shared_ptr<chemspace::ChemicalSpace> cs = chemspace::ChemicalSpace::deserialize(ifs);
+
+    auto dp =
+        datapipe::DataPipeline(cs,
+                               {{"ecfp4", descriptor::MorganFingerprint::ecfp4()},
+                                {"fcfp4", descriptor::MorganFingerprint::fcfp4()}},
+                               {{"synthesis", descriptor::SynthesisPostfixNotation::create(16)}});
+
+    dp.start_workers({1, 2, 3, 4});
+
+    for (int i = 0; i < 1000; ++i) {
+        auto start_time = std::chrono::high_resolution_clock::now();
+
+        auto batch = dp.get(1024);
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::high_resolution_clock::now() - start_time);
+        std::cout << "Batch " << i << " retrieved (" << duration.count() << " ms)" << "\n";
+    }
+
+    dp.stop_workers();
     return 0;
 }
